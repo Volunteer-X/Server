704169862937eb997bc41fb69aa16c9ac0ed75ff
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2023-12-12 14:38:14 +0000

    Refactor GqlAuthGuard and JwtStrategy

--- libs/auth/src/guard/gql.guard.ts
+++ libs/auth/src/guard/gql.guard.ts
@@ -6,6 +6,25 @@ import { AuthGuard } from '@nestjs/passport';
 export class GqlAuthGuard extends AuthGuard() {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
+
+    // console.log('ctx', ctx.getContext().req?.get('authorization'));
+
     return ctx.getContext().req;
+  }
+
+  handleRequest<TUser = any>(
+    err: any,
+    user: any,
+    info: any,
+    context: ExecutionContext,
+    status?: any,
+  ): TUser {
+    console.error('err', err);
+    console.log('user', user);
+    console.info('info', info);
+    console.log('context', context);
+    console.log('status', status);
+
+    return super.handleRequest(err, user, info, context, status);
   }
 }
--- libs/auth/src/jwt/jwt.strategy.ts
+++ libs/auth/src/jwt/jwt.strategy.ts
@@ -30,25 +30,16 @@ export class JwtStrategy extends PassportStrategy(BaseStrategy) {
         },
       }),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      // audience: configService.get<string>('AUTH0_AUDIENCE'),
+      audience: configService.get<string>('AUTH0_AUDIENCE'),
       issuer: `https://${configService.get<string>('AUTH0_DOMAIN')}/`,
       algorithms: ['RS256'],
     });
   }
 
   async validate(payload: JwtPayload): Promise<User> {
-    // const minimumScope = ['openid', 'profile', 'email'];
-
-    // if (
-    //   payload?.scope?.split(' ')
-    //     .filter((scope) => minimumScope.indexOf(scope) > -1).length !== 3
-    // ) {
-    //   throw new UnauthorizedException(
-    //     'JWT does not possess the required scope (`openid profile email`).',
-    //   );
-    // }
-
     // get user info from payload
+
+    console.log('payload', payload);
 
     const email = payload?.email;
 
